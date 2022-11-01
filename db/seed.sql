2fc152da7092b0f5fae1e85a5f9f760cc5d91e7b
Darx <mcrivabode@gmail.com>
2022-11-01 16:37:26 -0700

    add seed for role table

--- db/seed.sql
+++ db/seed.sql
@@ -2,6 +2,11 @@
 INSERT INTO department (name)
 VALUES
     ('Service'),
-    ('Sales'),
+    ('Sales');
 
--- 
+-- four tests for role table
+INSERT INTO role (title, salary, department_id)
+    ('Sales leader', 1500.50, 2),
+    ('Salesperson', 950.25, 2),
+    ('Accountant', 1300.00, 1),
+    ('Intern', 100.42, 1);
