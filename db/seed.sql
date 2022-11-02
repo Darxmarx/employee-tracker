3eb6dc1797fe9973cb826dcf2f02ef5714eb3a54
Darx <mcrivabode@gmail.com>
2022-11-01 20:32:23 -0700

    add seed for employee table

--- db/seed.sql
+++ db/seed.sql
@@ -10,3 +10,10 @@ INSERT INTO role (title, salary, department_id)
     ('Salesperson', 950.25, 2),
     ('Accountant', 1300.00, 1),
     ('Intern', 100.42, 1);
+
+-- four tests for employee table
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+    ('Kirby', 'Friendperson', 37, NULL), -- no manager
+    ('King', 'Dedede', 38, 1),
+    ('Waddle', 'Dee', 39, 2),
+    ('Meta', 'Knight', 40, NULL) -- no manager
