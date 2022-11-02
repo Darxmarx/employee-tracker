0f559a7c46712b57effad215e18a5f9ad8ee04e9
Darx <mcrivabode@gmail.com>
2022-11-01 20:48:40 -0700

    add choices available inside the index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const { default: inquirer } = require('inquirer');
 
 // connect to the mysql database
 const connection = mysql.createConnection({
@@ -12,3 +13,24 @@ const connection = mysql.createConnection({
     database: 'employee_db'
 });
 
+// function for initial prompt, asking user to make a choice between the features
+const employeeTracker = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'firstChoice',
+            message: 'What would you like to do?',
+            // all available choices for use in this application
+            choices: [
+                'View all Employees',
+                'View Departments',
+                'View Roles',
+                'Add an Employee',
+                'Add a Department',
+                'Add a Role',
+                'Update an Employee Role',
+                'Exit'
+            ]
+        }
+    ])
+}
